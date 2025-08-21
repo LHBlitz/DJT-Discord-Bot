b7e2744c099eb245a86b6765d50f7b84f3433237
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-08-20 21:07:20 -0400

    added Israel support
    $12 billion

---
+++ Commands/Utility/israel.js
@@ -0,0 +1,12 @@
+// Commands/Utility/israel.js
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('israel')
+		.setDescription('Remind the Don to send Israel more money'),
+	async execute(interaction) {
+		await interaction.reply('Yes, Israel! Here Netanyahu, this $12 Billion is yours!');
+	},
+};
