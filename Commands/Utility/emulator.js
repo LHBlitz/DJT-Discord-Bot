78f06242f1b3eba6e46f375c89d182734275ca36
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-10-03 04:12:35 -0400

    New command and updates
    Added new command "Emulator" and updated the command "Quests".

---
+++ Commands/Utility/emulator.js
@@ -0,0 +1,56 @@
+// Commands/Utility/emulator.js
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+        data: new SlashCommandBuilder()
+        .setName('emulators')
+        .setDescription('Emulators you should be using.'),
+    async execute(interaction) {
+
+        const reply = `
+        
+        This list is only for emulators that function on a PC. Usage varies and most support both Windows and Linux. If something isn't listed, I have a list on Github for various other emulators and stuff related to it.
+https://github.com/stars/LHBlitz/lists/emulation-stuff
+
+Before you ask, no there aren't any emulators for PS5 and Xbox one/Series consoles.
+
+Multi system:
+Ares - https://ares-emu.net/
+RetroArch - https://www.retroarch.com/
+Use this for systems released up to the DS. Supports all the old school systems. RetroArch is best used if you want to unlock Achievements.
+See more here -https://retroachievements.org/
+
+Individual:
+PlayStation
+PS1 - https://www.duckstation.org/
+PS2 - https://pcsx2.net/
+PS3 - https://rpcs3.net/
+PS4 - https://shadps4.net/
+PSP - https://www.ppsspp.org/
+PS Vita - https://vita3k.org/
+
+Xbox
+OG Xbox - https://xemu.app/
+Xbox 360 - https://xenia.jp/
+
+Nintendo
+Wii/GameCube - https://dolphin-emu.org/
+DS - https://melonds.kuribo64.net/
+3DS - https://github.com/azahar-emu/azahar
+Wii U - https://cemu.info/
+Switch:
+https://github.com/Ryubing/
+https://github.com/Kenji-NX/
+
+Sega
+Genesis - https://github.com/ekeeke/Genesis-Plus-GX
+Dreamcast - https://flyinghead.github.io/flycast-builds/#
+Saturn - https://www.yabasanshiro.com/');
+        
+        `;
+
+
+        await interaction.reply(reply);
+    },
+};
--- Commands/Utility/quests.js
+++ Commands/Utility/quests.js
@@ -3,10 +3,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('quests')
-		.setDescription('Use this code to automatically complete Discord Quests.'),
-	async execute(interaction) {
-		await interaction.reply('https://gist.github.com/aamiaa/204cd9d42013ded9faf646fae7f89fbb');
-	},
+    data: new SlashCommandBuilder()
+        .setName('quests')
+        .setDescription('Use this code to automatically complete Discord Quests.'),
+    async execute(interaction) {
+        await interaction.reply('https://gist.github.com/aamiaa/204cd9d42013ded9faf646fae7f89fbb');
+    },
 };
