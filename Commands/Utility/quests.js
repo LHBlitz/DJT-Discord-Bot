d47fbba524dd8c99304a5f119e8d7a12806125e5
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-10-13 18:06:49 -0400

    sync updates

--- Commands/Utility/quests.js
+++ Commands/Utility/quests.js
@@ -3,10 +3,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
+        data: new SlashCommandBuilder()
         .setName('quests')
-        .setDescription('Use this code to automatically complete Discord Quests.'),
+        .setDescription('Use this script to automatically complete Discord Quests.'),
     async execute(interaction) {
-        await interaction.reply('https://gist.github.com/aamiaa/204cd9d42013ded9faf646fae7f89fbb');
+
+        const reply = `
+
+Note: This script requires the Discord desktop app. It doesn't work on the web, mobile and Vencord browser based app.
+https://gist.github.com/aamiaa/204cd9d42013ded9faf646fae7f89fbb
+        `;
+
+
+        await interaction.reply(reply);
     },
 };
