f8f5386b69dcde468f4c1e3690710b38bded6d88
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-10-15 12:29:55 -0400

    updated About command

--- Commands/Utility/about.js
+++ Commands/Utility/about.js
@@ -10,13 +10,18 @@ module.exports = {
 
         const reply = `
 
-This is a bot made for a private friend based Discord server. Its purpose is to be a tool bot on the likes of MEE6 and Dyno, but with a twist of humor inspired by President Donald J. Trump. It is privately held and ran.
+A multipurpose Discord bot paired with administrative tools and humor.
 
-Disclaimer - The functions and humor used in this bot do no reflect the values and views of the creators. It is entirely based on subjective humor relating to the person or persons the bot is based on.
+This is a bot made for a private friend based Discord server. Its purpose is to be a tool bot on the likes of MEE6 and Dyno, but with a twist of humor
+inspired by President Donald J. Trump. It is privately held and ran.
+
+Disclaimer - The functions and humor used in this bot do no reflect the values and views of the creators. It is entirely based on subjective humor
+relating to the person or persons the bot is based on.
 
 Contributing:
-This bot is privately held between the individuals Zanzort and LHBlitz for a private Discord group dedicated to our friends. Therefore, there will be no public contributions outside of these two individuals.
-Any other potential contributions will involve individuals we are familiar with. If you are friends with us, you can DM Zanzort or LHBlitz on Discord to discuss getting involved with the project.
+This bot is privately held between the individuals Zanzort and LHBlitz for a private Discord group dedicated to our friends. Therefore, there will be no public contributions
+outside of these two individuals. Any other potential contributions will involve individuals we are familiar with. If you are friends with us, you can DM Zanzort or LHBlitz
+on Discord to discuss getting involved with the project.
 
 MIT License:
 
