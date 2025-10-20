30b7d0ed6912617feef232e8d4580a4aeb415104
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-10-20 13:10:19 -0400

    updated command
    New text and updated error message
    
    Signed-off-by: Ken <52684990+LHBlitz@users.noreply.github.com>

--- Commands/Utility/trumpsays.js
+++ Commands/Utility/trumpsays.js
@@ -4,12 +4,12 @@ const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('trumpsays')
-        .setDescription('Listen to what the Don has to say'),
+        .setDescription('Listen to what the Don has to say.'),
 
     async execute(interaction) {
         // Read all MP3 files in the audio folder
         const clips = fs.readdirSync('./audio').filter(file => file.endsWith('.mp3'));
-        if (clips.length === 0) return interaction.reply("No audio clips found!");
+        if (clips.length === 0) return interaction.reply("America LOVES to hear me talk, but I have nothing to say right now.");
 
         // Pick a random clip
         const clipName = clips[Math.floor(Math.random() * clips.length)];
