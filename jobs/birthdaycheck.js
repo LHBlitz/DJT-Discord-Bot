4aeaa5cdf2e54ce159c6c4153302516f48424a1e
Ken <52684990+LHBlitz@users.noreply.github.com>
2025-12-16 01:51:13 -0500

    Enhance birthday messages with age display
    Added age calculation to birthday messages.
    
    Signed-off-by: Ken <52684990+LHBlitz@users.noreply.github.com>

--- jobs/birthdaycheck.js
+++ jobs/birthdaycheck.js
@@ -1,23 +1,17 @@
 const cron = require("node-cron");
-const {
-  getBirthdaysForGuild,
-  markCelebrated
-} = require("../data/birthdays");
+const { getBirthdaysForGuild, markCelebrated } = require("../data/birthdays");
 
 module.exports = function startBirthdayJob(client) {
-  cron.schedule("*/10 * * * *", async () => {
+  cron.schedule("* * * * *", async () => {
     const nowUTC = new Date();
 
     for (const guild of client.guilds.cache.values()) {
       const birthdays = getBirthdaysForGuild(guild.id);
 
-      const announcementChannel =
-      guild.channels.cache.get("1375293339615891549");
-      
+      const announcementChannel = guild.channels.cache.get("1375293339615891549");
       if (!announcementChannel) continue;
 
-      const birthdayRole =
-        guild.roles.cache.find(r => r.name === "Birthday");
+      const birthdayRole = guild.roles.cache.find(r => r.name === "Birthday");
 
       for (const b of birthdays) {
         const local = new Date(
@@ -33,9 +27,13 @@ module.exports = function startBirthdayJob(client) {
         const member = await guild.members.fetch(b.userId).catch(() => null);
         if (!member) continue;
 
-        await announcementChannel.send(
-          `**Happy Birthday, ${member}!**`
-        );
+        const age = b.year ? local.getFullYear() - b.year : null;
+
+        const message = age
+          ? `**Happy ${age}th Birthday, ${member}!**`
+          : `**Happy Birthday, ${member}!**`;
+
+        await announcementChannel.send(message);
 
         if (birthdayRole) {
           await member.roles.add(birthdayRole).catch(() => {});
